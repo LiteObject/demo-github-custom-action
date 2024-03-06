e5533cbdbaa4fffd1244c9bd42760742b81d9905
LiteObject <Lite.Object@gmail.com>
2024-03-06 15:40:59 -0600

    Update cron schedule and print statement with datetime

--- action.py
+++ action.py
@@ -1,12 +1,13 @@
 import sys
 import os
 import json
+import datetime
 
 def main():
   name = 'World'
   user_input =  name = os.environ['INPUT_PERSON']
   print(f"user_input: {user_input}")
-  
+
   if 'GITHUB_EVENT_PATH' in os.environ:
     event_path = os.environ['GITHUB_EVENT_PATH']
     with open(event_path) as fp:
@@ -14,7 +15,7 @@ def main():
       if 'pull_request' in event:
         name = event['pull_request']['head']['repo']['name']
 
-  print(f'Hello {name}!')
+  print(f'[{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Hello {name}!')
 
 if __name__ == '__main__':
   main()
