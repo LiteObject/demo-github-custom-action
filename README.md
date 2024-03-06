ad7949e390ddec2aec804e483ab81abd7cb37450
LiteObject <Lite.Object@gmail.com>
2024-03-05 22:23:08 -0600

    Add initial files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# demo-github-custom-action
+# Demo Github Custom Action
+
+
+---
+## Links
+- [GitHub: Creating actions](https://docs.github.com/en/actions/creating-actions)
---
+++ action.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import json
+
+def main():
+  name = 'World'
+  user_input =  name = os.environ['INPUT_PERSON']
+  print(f"user_input: {user_input}")
+  if 'GITHUB_EVENT_PATH' in os.environ:
+    event_path = os.environ['GITHUB_EVENT_PATH']
+    with open(event_path) as fp:
+      event = json.load(fp)
+      if 'pull_request' in event:
+        name = event['pull_request']['head']['repo']['name']
+
+  print(f'Hello {name}!')
+
+if __name__ == '__main__':
+  main()
